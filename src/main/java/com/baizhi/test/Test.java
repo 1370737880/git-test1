ac2b0374ee94217069e89a3cdc563288ac4ae1ba
git <1370737880@qq.com>
2018-10-19 21:05:09 +0800

    this is dev5

--- src/main/java/com/baizhi/test/Test.java
+++ src/main/java/com/baizhi/test/Test.java
@@ -6,7 +6,9 @@ package com.baizhi.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("我是dev");
+        System.out.println("你好");
         System.out.println("我是dev");
+
 
     }
 }
